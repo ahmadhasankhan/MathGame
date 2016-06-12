609aaa76ccb470b68c72ef34d9ffb2e7d9f2bac9
Ahmad Hassan <er.ahmad.hassan@gmail.com>
2016-06-13 02:53:14 +0530

    first commit

---
+++ app/src/main/java/com/sd3lab/ahmadhassan/mathgamechapter2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sd3lab.ahmadhassan.mathgamechapter2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
