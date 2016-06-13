36f4a990e95018d5067c9c56600cf64187930f5c
Ahmad Hassan <er.ahmad.hassan@gmail.com>
2016-06-14 04:53:18 +0530

    Game activity initial design

--- app/src/main/java/com/sd3lab/ahmadhassan/mathgamechapter2/GameActivity.java
+++ app/src/main/java/com/sd3lab/ahmadhassan/mathgamechapter2/GameActivity.java
@@ -3,11 +3,11 @@ package com.sd3lab.ahmadhassan.mathgamechapter2;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class GameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
     }
 }
