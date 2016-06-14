d6ad4012fa95901fc74d69b295fefa839d091056
Ahmad Hassan <er.ahmad.hassan@gmail.com>
2016-06-14 05:44:56 +0530

    On Click nevigated to game activity

--- app/src/main/java/com/sd3lab/ahmadhassan/mathgamechapter2/GameActivity.java
+++ app/src/main/java/com/sd3lab/ahmadhassan/mathgamechapter2/GameActivity.java
@@ -2,6 +2,8 @@ package com.sd3lab.ahmadhassan.mathgamechapter2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GameActivity extends Activity {
 
@@ -9,5 +11,24 @@ public class GameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        int partA = 9;
+        int partB = 9;
+        int correctAnswer = partA * partB;
+        int wrongAnswer1 = partA * partB + 1;
+        int wrongAnswer2 = partA * partB - 1;
+
+        TextView textObjectPartA = (TextView) findViewById(R.id.textPartA);
+        TextView textObjectPartB = (TextView) findViewById(R.id.textPartB);
+        Button buttonObjectChoice1 = (Button) findViewById(R.id.buttonChoice1);
+        Button buttonObjectChoice2 = (Button) findViewById(R.id.buttonChoice2);
+        Button buttonObjectChoice3 = (Button) findViewById(R.id.buttonChoice3);
+
+        textObjectPartA.setText("" + partA);
+        textObjectPartB.setText("" + partB);
+        buttonObjectChoice1.setText("" + correctAnswer);
+        buttonObjectChoice2.setText("" + wrongAnswer1);
+        buttonObjectChoice3.setText("" + wrongAnswer2);
+
     }
 }
